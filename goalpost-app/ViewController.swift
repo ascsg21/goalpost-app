bed6ff490075916d6f31408fd5b1350a2146c51b
MinRiDaddy <lovelyroom77@gmail.com>
2018-07-29 13:56:30 +0900

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by MinRiDaddy on 2018. 7. 29..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
