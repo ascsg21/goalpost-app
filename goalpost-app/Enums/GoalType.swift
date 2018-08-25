050ff9f874761db2aab552f91d2bc04d446e3181
MinRiDaddy <lovelyroom77@gmail.com>
2018-08-25 16:40:31 +0900

    8월 25일 commit

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by MinRiDaddy on 2018. 8. 25..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
