96623ef348436c4f678422119d989a40a10fd747
MinRiDaddy <lovelyroom77@gmail.com>
2018-08-26 14:50:31 +0900

    8월 26일 commit

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,48 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by MinRiDaddy on 2018. 8. 26..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    var goalType: GoalType = .shortTerm
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nextBtn.bindToKeyborad()
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
+    }
+    
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermBtn.setSelectedColor()
+        shortTermBtn.setDeselectedColor()
+    }
+    
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
+}
